71d6f4d3b0422faa2417edb21d09edaa505923c9
Aaron Krajeski <krajeski@gmail.com>
2021-10-17 14:02:24 -0400

    Bookmark, gonna always return new

--- ComplexNumber.js
+++ ComplexNumber.js
@@ -4,3 +4,22 @@ class ComplexNumber {
     this.imaginary = imaginary;
   }
 }
+
+
+function multiplyComplex(A, B) {
+  const a = A.real * B.real - A.imaginary * B.imaginary;
+  const i = A.real * B.imaginary + A.imaginary * B.real;
+  return new ComplexNumber(a, i);
+}
+
+function addComplex(A, B) {
+  return new ComplexNumber(A.real + B.real, A.imaginary + B.imaginary);
+}
+
+function complexPower(x, n) {
+  const result = new ComplexNumber(x.real, x.imaginary);
+  for (let i = 0; i < n - 1; i++) {
+    result = multiplyComplex(result, result);
+  }
+  return result;
+}
